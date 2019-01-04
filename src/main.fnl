eb2776c04fe4a96c37cb5c8d0642849de3aa34c4
Ray Perry <rmperry09@gmail.com>
2019-01-03 20:47:48 -0600

    Initial commit.

---
+++ src/main.fnl
@@ -0,0 +1,198 @@
+;; -*- compile-command: "fennel --compile main.fnl > main.lua && love ." -*-
+
+(local (lg lk le la)
+       (values
+        love.graphics
+        love.keyboard
+        love.event
+        love.audio))
+
+(local lume (require :lume))
+(local flux (require :flux))
+
+(local scene-key-map { })
+(local movement-key-map { :left [:x -1] :right [:x 1] :up [:y -1] :down [:y 1] })
+
+(local player { :x 32 :y 32 :radius 0 :opacity 1 :speed 50 })
+(local ring { :radius 500 :opacity 1 })
+(local gamestate { :status :STARTING :status-hooks [] :timers [] :load [] :update [] :draw [] :keypressed [] })
+(local gong (la.newSource "bowl.wav" :static))
+(: gong :setVolume 0.8)
+
+(fn start-reluctance []
+    (: (flux.to player 5 { :x ring.x :y ring.y :opacity 1 }) :ease :cubicout))
+
+(var reluctance (start-reluctance))
+
+(fn register-callback [callback-type callback]
+    (if callback-type
+        (let [number-of-callbacks
+              (# (. gamestate callback-type))
+              message
+              (..
+               "Added callback #"
+                (+ number-of-callbacks 1)
+                " to '"
+                callback-type
+                "'.")]
+          (table.insert (. gamestate callback-type) callback))
+        (callback))
+    (# (. gamestate callback-type)))
+
+(fn unregister-callback [callback-type index]
+    (if index
+        (table.remove (. gamestate callback-type) index)
+        (table.remove (. gamestate callback-type))))
+
+(fn run-callbacks [list-of-callbacks ...]
+    (each [key callback (pairs list-of-callbacks)]
+          (when (= (type callback) :function)
+            (callback (unpack [...])))))
+
+(fn set-gamestate-status [state]
+    (set gamestate.status state)
+    (run-callbacks gamestate.status-hooks))
+
+(fn quit-game [] (set-gamestate-status :STOPPING))
+
+(fn move-player [axis direction dt]
+    (: reluctance :stop)
+    (let [[width height] [(lg.getDimensions)]]
+      (local new-value (+ (. player axis) (* direction (* player.speed player.opacity) dt)))
+      (when (not (= gamestate.status :COMPLETE))
+        (tset player axis new-value))
+      (when (or (< (+ player.x player.radius) 0)
+                (< (+ player.y player.radius) 0)
+                (> (- player.x player.radius) width)
+                (> (- player.y player.radius) height))
+        (set-gamestate-status :COMPLETE)))
+
+    (set player.opacity
+         (lume.clamp (- 1 (/ (- (lume.distance player.x player.y ring.x ring.y) ring.radius) (* ring.radius 1.15))) 0 1))
+
+    (when (and (<= player.opacity 0.2)
+               (not (= gamestate.status :COMPLETE)))
+      (: reluctance :stop)
+      (: (flux.to ring 1 { :radius (* ring.radius 1.5) :opacity (- ring.opacity 0.2) })
+         :ease :cubicout)
+      (: (flux.to player 0.5 { :x ring.x :y ring.y :opacity 1 :radius (* player.radius 0.9) :speed (* player.speed 1.1) })
+         :ease :cubicout)))
+
+(fn player.draw []
+    (lg.setColor 0.16 0.16 0.16 player.opacity)
+    (lg.circle :fill player.x player.y player.radius)
+    (lg.setColor 0 0 0))
+
+(fn ring.draw []
+    (lg.setColor 0 0 0 ring.opacity)
+    (lg.circle :line ring.x ring.y ring.radius)
+    (lg.setColor 0 0 0))
+
+(fn scene-update [dt]
+    (flux.update dt)
+    (each [key action (pairs (lume.extend scene-key-map movement-key-map))]
+          (table.insert action dt)
+          (when (and (lk.isDown key) (> ring.radius player.radius))
+            (if (= (type action) :function)
+                (action)
+                (move-player (unpack action))))))
+
+(fn scene-draw []
+    (lg.setBackgroundColor 0.917 0.917 0.917)
+    (ring.draw)
+    (player.draw))
+
+(fn scene-keypressed [key]
+    (when (~= key nil) (: reluctance :stop))
+    (when (= key :c) (set-gamestate-status :COMPLETE)))
+
+(local main-scene { :update scene-update :draw scene-draw :keypressed scene-keypressed })
+
+(local global-key-map { })
+;; ===============
+
+(fn love.keypressed [key]
+    (when (= key :p)
+        (if (= gamestate.status :RUNNING)
+            (set-gamestate-status :PAUSED)
+            (set-gamestate-status :RUNNING)))
+    (when (= key :q) (quit-game))
+    (run-callbacks gamestate.keypressed key))
+
+(fn love.load [] 
+    (register-callback
+     :load
+     (fn first-load []
+         (register-callback
+          :status-hooks
+          (fn game-started []
+              (when (= gamestate.status :STARTED)
+                (set-gamestate-status :RUNNING)
+                (: (flux.to player 1 { :radius 28 })
+                   :after
+                   ring 1 { :radius (* 32 1.15) }))))
+
+         (register-callback
+          :status-hooks
+          (fn game-stopping []
+              (when (= gamestate.status :STOPPING)
+                (set-gamestate-status :STOPPED))))
+
+         (register-callback
+          :status-hooks
+          (fn game-stopped []
+              (when (= gamestate.status :STOPPED)
+                (le.quit))))
+
+         (register-callback
+          :status-hooks
+          (fn game-complete []
+              (when (= gamestate.status :COMPLETE)
+                (: reluctance :stop)
+                (la.play gong)
+                (flux.to ring 2 { :opacity 0 })
+                (flux.to player 2 { :opacity 0 }))))
+              
+         (register-callback
+          :update
+          (fn key-handler []
+              (each [key callback (pairs global-key-map)]
+                    (when (and (lk.isDown key)
+                               (= (type callback) :function))
+                      (callback)))))
+
+         (register-callback
+          :update
+          (fn upon-completion [dt]
+              (when (and (= gamestate.status :COMPLETE) (: gong :isPlaying))
+                (: gong :setVolume (- (: gong :getVolume) (/ dt 4)))
+                (when (<= (: gong :getVolume) 0.01)
+                  (: gong :stop)
+                  (quit-game)))))
+
+         (let [[width height] [(lg.getDimensions)]]
+           (set ring.x (math.floor (/ width 2)))
+           (set ring.y (math.floor (/ height 2)))
+           (set player.x ring.x)
+           (set player.y ring.y))
+
+         (register-callback :update main-scene.update)
+         (register-callback :draw main-scene.draw)
+         (register-callback :keypressed main-scene.keypressed)
+         
+         (set-gamestate-status :STARTED)))
+    
+    (run-callbacks gamestate.load))
+
+(fn love.update [dt]
+    (when (not (= gamestate.status :PAUSED))
+      (run-callbacks gamestate.update dt)))
+
+(fn love.draw [] (run-callbacks gamestate.draw))
+
+(fn love.keyreleased []
+    (when (not (= gamestate.status :COMPLETE))
+      (set reluctance (start-reluctance))))
+
+
+
---
+++ src/main.lua
@@ -0,0 +1,228 @@
+local lg, lk, le, la = love.graphics, love.keyboard, love.event, love.audio
+local lume = require("lume")
+local flux = require("flux")
+local scene_key_map = {}
+local movement_key_map = {down = {"y", 1}, left = {"x", -1}, right = {"x", 1}, up = {"y", -1}}
+local player = {opacity = 1, radius = 0, speed = 50, x = 32, y = 32}
+local ring = {opacity = 1, radius = 500}
+local gamestate = {["status-hooks"] = {}, draw = {}, keypressed = {}, load = {}, status = "STARTING", timers = {}, update = {}}
+local gong = la.newSource("bowl.wav", "static")
+gong:setVolume(0.80000000000000004)
+local function start_reluctance()
+  return flux.to(player, 5, {opacity = 1, x = ring.x, y = ring.y}):ease("cubicout")
+end
+local reluctance = start_reluctance()
+local function register_callback(callback_type, callback)
+  local function _0_()
+    if callback_type then
+      local number_of_callbacks = #gamestate[callback_type]
+      local message = ("Added callback #" .. (number_of_callbacks + 1) .. " to '" .. callback_type .. "'.")
+      return table.insert(gamestate[callback_type], callback)
+    else
+      return callback()
+    end
+  end
+  _0_()
+  return #gamestate[callback_type]
+end
+local function unregister_callback(callback_type, index)
+  if index then
+    return table.remove(gamestate[callback_type], index)
+  else
+    return table.remove(gamestate[callback_type])
+  end
+end
+local function run_callbacks(list_of_callbacks, ...)
+  for key, callback in pairs(list_of_callbacks) do
+    local function _0_(...)
+      if (type(callback) == "function") then
+        return callback(unpack({...}))
+      end
+    end
+    _0_(...)
+  end
+  return nil
+end
+local function set_gamestate_status(state)
+  gamestate.status = state
+  return run_callbacks(gamestate["status-hooks"])
+end
+local function quit_game()
+  return set_gamestate_status("STOPPING")
+end
+local function move_player(axis, direction, dt)
+  reluctance:stop()
+  do
+    local _0_ = {lg.getDimensions()}
+    local width = _0_[1]
+    local height = _0_[2]
+    local new_value = (player[axis] + (direction * (player.speed * player.opacity) * dt))
+    local function _1_()
+      if not (gamestate.status == "COMPLETE") then
+        player[axis] = new_value
+        return nil
+      end
+    end
+    _1_()
+    local function _2_()
+      if (((player.x + player.radius) < 0) or ((player.y + player.radius) < 0) or ((player.x - player.radius) > width) or ((player.y - player.radius) > height)) then
+        return set_gamestate_status("COMPLETE")
+      end
+    end
+    _2_()
+  end
+  player.opacity = lume.clamp((1 - ((lume.distance(player.x, player.y, ring.x, ring.y) - ring.radius) / (ring.radius * 1.1499999999999999))), 0, 1)
+  if ((player.opacity <= 0.20000000000000001) and not (gamestate.status == "COMPLETE")) then
+    reluctance:stop()
+    flux.to(ring, 1, {opacity = (ring.opacity - 0.20000000000000001), radius = (ring.radius * 1.5)}):ease("cubicout")
+    return flux.to(player, 0.5, {opacity = 1, radius = (player.radius * 0.90000000000000002), speed = (player.speed * 1.1000000000000001), x = ring.x, y = ring.y}):ease("cubicout")
+  end
+end
+player.draw = function()
+  lg.setColor(0.16, 0.16, 0.16, player.opacity)
+  lg.circle("fill", player.x, player.y, player.radius)
+  return lg.setColor(0, 0, 0)
+end
+ring.draw = function()
+  lg.setColor(0, 0, 0, ring.opacity)
+  lg.circle("line", ring.x, ring.y, ring.radius)
+  return lg.setColor(0, 0, 0)
+end
+local function scene_update(dt)
+  flux.update(dt)
+  for key, action in pairs(lume.extend(scene_key_map, movement_key_map)) do
+    table.insert(action, dt)
+    local function _0_()
+      if (lk.isDown(key) and (ring.radius > player.radius)) then
+        if (type(action) == "function") then
+          return action()
+        else
+          return move_player(unpack(action))
+        end
+      end
+    end
+    _0_()
+  end
+  return nil
+end
+local function scene_draw()
+  lg.setBackgroundColor(0.91700000000000004, 0.91700000000000004, 0.91700000000000004)
+  ring.draw()
+  return player.draw()
+end
+local function scene_keypressed(key)
+  local function _0_()
+    if (key ~= nil) then
+      return reluctance:stop()
+    end
+  end
+  _0_()
+  if (key == "c") then
+    return set_gamestate_status("COMPLETE")
+  end
+end
+local main_scene = {draw = scene_draw, keypressed = scene_keypressed, update = scene_update}
+local global_key_map = {}
+love.keypressed = function(key)
+  local function _0_()
+    if (key == "p") then
+      if (gamestate.status == "RUNNING") then
+        return set_gamestate_status("PAUSED")
+      else
+        return set_gamestate_status("RUNNING")
+      end
+    end
+  end
+  _0_()
+  local function _1_()
+    if (key == "q") then
+      return quit_game()
+    end
+  end
+  _1_()
+  return run_callbacks(gamestate.keypressed, key)
+end
+love.load = function()
+  local function first_load()
+    local function game_started()
+      if (gamestate.status == "STARTED") then
+        set_gamestate_status("RUNNING")
+        return flux.to(player, 1, {radius = 28}):after(ring, 1, {radius = (32 * 1.1499999999999999)})
+      end
+    end
+    register_callback("status-hooks", game_started)
+    local function game_stopping()
+      if (gamestate.status == "STOPPING") then
+        return set_gamestate_status("STOPPED")
+      end
+    end
+    register_callback("status-hooks", game_stopping)
+    local function game_stopped()
+      if (gamestate.status == "STOPPED") then
+        return le.quit()
+      end
+    end
+    register_callback("status-hooks", game_stopped)
+    local function game_complete()
+      if (gamestate.status == "COMPLETE") then
+        reluctance:stop()
+        la.play(gong)
+        flux.to(ring, 2, {opacity = 0})
+        return flux.to(player, 2, {opacity = 0})
+      end
+    end
+    register_callback("status-hooks", game_complete)
+    local function key_handler()
+      for key, callback in pairs(global_key_map) do
+        local function _0_()
+          if (lk.isDown(key) and (type(callback) == "function")) then
+            return callback()
+          end
+        end
+        _0_()
+      end
+      return nil
+    end
+    register_callback("update", key_handler)
+    local function upon_completion(dt)
+      if ((gamestate.status == "COMPLETE") and gong:isPlaying()) then
+        gong:setVolume((gong:getVolume() - (dt / 4)))
+        if (gong:getVolume() <= 0.01) then
+          gong:stop()
+          return quit_game()
+        end
+      end
+    end
+    register_callback("update", upon_completion)
+    do
+      local _0_ = {lg.getDimensions()}
+      local width = _0_[1]
+      local height = _0_[2]
+      ring.x = math.floor((width / 2))
+      ring.y = math.floor((height / 2))
+      player.x = ring.x
+      player.y = ring.y
+    end
+    register_callback("update", main_scene.update)
+    register_callback("draw", main_scene.draw)
+    register_callback("keypressed", main_scene.keypressed)
+    return set_gamestate_status("STARTED")
+  end
+  register_callback("load", first_load)
+  return run_callbacks(gamestate.load)
+end
+love.update = function(dt)
+  if not (gamestate.status == "PAUSED") then
+    return run_callbacks(gamestate.update, dt)
+  end
+end
+love.draw = function()
+  return run_callbacks(gamestate.draw)
+end
+love.keyreleased = function()
+  if not (gamestate.status == "COMPLETE") then
+    reluctance = start_reluctance()
+    return nil
+  end
+end
+return love.keyreleased
